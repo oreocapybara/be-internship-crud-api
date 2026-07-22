795dded59a5a9835c218e04cd4e0b75dc57fc93b
oreocapybara <71880256+oreocapybara@users.noreply.github.com>
2026-07-22 09:40:10 +0800

    Stage 1: root and health endpoints

--- src/app.js
+++ src/app.js
@@ -2,10 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//STAGE 1
+//Add the endpoint GET / returning JSON that describes your API:
 app.get("/", (req, res) => {
-	res.send("Hello World!");
+	res.send({ name: "Task API", version: "1.0", endpoints: ["/tasks"] });
 });
 
+app.get("/health", (req, res) => {
+	res.status(200).send({ "status": "ok" })
+});
+
+//Add GET /health returning { "status": "ok" }
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
