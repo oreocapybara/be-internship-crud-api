e594d82c004beb531ba455059d6d1434515664b8
oreocapybara <71880256+oreocapybara@users.noreply.github.com>
2026-07-28 15:47:20 +0800

    STAGE 0: create SQLite Database

--- src/repositories/tasks.repo.js
+++ src/repositories/tasks.repo.js
@@ -1,20 +1,41 @@
-const SEED_TASKS = [
-	{ id: 1, title: "Do Laundry", done: false },
-	{ id: 2, title: "Fix Laptop", done: true },
-	{ id: 3, title: "Have a Video Chat", done: false },
-];
+const Database = require("better-sqlite3");
+const db = new Database("./src/repositories/tasks.db");
 
-const tasks = SEED_TASKS.map((task) => ({ ...task }));
+//Create a database table if it does not exist
+db.exec(`CREATE TABLE IF NOT EXISTS tasks(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		title TEXT NOT NULL,
+		done INTEGER DEFAULT 0
+		)`);
 
-const findAll = () => {
-	return tasks.map((task) => ({ ...tasks }));
-};
+//SEED database with tasks when empty;
+const row = db.prepare(`SELECT COUNT(*) AS count FROM tasks`).get();
+
+if (row.count === 0) {
+	const insert = db.prepare(`INSERT INTO tasks(title, done) VALUES(?, ?)`);
+
+	const SEED_TASKS = [
+		["Do Laundry", 0],
+		["Fix Laptop", 1],
+		["Have a Video Chat", 0],
+	];
+
+	const insertSeedTasks = db.transaction((tasks) => {
+		for (const task of tasks) insert.run(task);
+	});
+
+	insertSeedTasks(SEED_TASKS);
+}
+
+function findAll() {
+	return db.prepare(`SELECT * FROM tasks`).all();
+}
 
 const findTask = (id) => {
-	return tasks.find((task) => (task.id === id ? { ...task } : null));
+	return db.prepare(`SELECT * FROM tasks WHERE id = ?`).get(id);
+	// return tasks.find((task) => (task.id === id ? { ...task } : null));
 };
 
-console.log(findTask(4))
 const create = ({ title, done }) => {
 	const id =
 		tasks.length === 0 ? 1 : Math.max(...tasks.map((task) => task.id)) + 1;
